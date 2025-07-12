1fd7dcf2ec59ab8bf24f3645544bc221ef523267
sapphiregaze <jasmine@sapphiregaze.dev>
2025-07-11 18:51:16 -0700

    Initialized Monorepo With Client App: - Initialized monorepo with turborepo - Created client app with React + Vite + Tanstack Router - Added pre-commit hook with husky

---
+++ apps/client/src/main.tsx
@@ -0,0 +1,35 @@
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+
+import { routeTree } from "./routeTree.gen";
+
+import "./styles.css";
+import reportWebVitals from "./reportWebVitals.ts";
+
+const router = createRouter({
+  routeTree,
+  context: {},
+  defaultPreload: "intent",
+  scrollRestoration: true,
+  defaultStructuralSharing: true,
+  defaultPreloadStaleTime: 0,
+});
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+const rootElement = document.getElementById("app");
+if (rootElement && !rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>,
+  );
+}
+
+reportWebVitals();
